96bdabf03d97702fea878d189bc5c193045b01dc
Aditya Maheshwari <amaheshwari819@gmail.com>
2025-08-12 18:12:27 +0530

    removing experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import styles from "./App.module.css";
 import { About } from "./components/About/About";
 import { Contact } from "./components/Contact/Contact";
-import Experience from "./components/Experience/Experience";
+// import Experience from "./components/Experience/Experience";
 import { Hero } from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Projects } from "./components/Projects/Projects";
@@ -14,7 +14,7 @@ function App() {
       <Hero />
       <About />
       <TechStack />
-      <Experience/>
+       
       <Projects />
       <Contact />
 
