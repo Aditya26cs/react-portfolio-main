cafd0a2307f1896793cf8e7d6e61459775c9fbd7
Aditya Maheshwari <amaheshwari819@gmail.com>
2025-11-22 13:40:57 +0530

    final check

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/", // IMPORTANT for Vercel deployment
+
   css: {
     modules: {
       localsConvention: "camelCase",
     },
   },
-  server: {
-    host: true, // allow access from network
-    allowedHosts: [
-       'ebb50e04539c.ngrok-free.app',
-    ]
-  }
 });
